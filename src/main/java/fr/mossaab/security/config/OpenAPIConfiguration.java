e8c93ecb20a54ac95f19b97be48864defd1b9544
Kichmarevitmo <kichmarev2003@gmail.com>
2024-12-22 19:34:35 +0300

    2 step

--- src/main/java/fr/mossaab/security/config/OpenAPIConfiguration.java
+++ src/main/java/fr/mossaab/security/config/OpenAPIConfiguration.java
@@ -83,7 +83,7 @@ public class OpenAPIConfiguration {
 
     @Bean
     public OpenApiCustomizer openApiCustomizer() {
-        return openApi -> openApi.addServersItem(new io.swagger.v3.oas.models.servers.Server().url("http://31.129.102.70:8080"));
+        return openApi -> openApi.addServersItem(new io.swagger.v3.oas.models.servers.Server().url("http://31.129.102.70:8081"));
     }
 
     /**
